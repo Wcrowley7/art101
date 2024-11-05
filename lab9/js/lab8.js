593006df47dbbd238ef432ac52464416afe6c017
Wcrowley7 <wcrowls19@gmail.com>
2024-11-04 20:17:20 -0800

    9
    9

--- lab9/js/lab8.js
+++ lab9/js/lab8.js
@@ -3,3 +3,24 @@
 // Date: October 30th, 2024
 // License: Public Domain
 
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+$("#button-challenge").click(function(){
+    $(this).parent().toggleClass("special");
+
+});
+
+
+
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+
+$("#button-problems").click(function(){
+$("#problems").toggleClass("special");
+});
+
+
+$("#results").append("<button id='button-results'>Make Special</button>");
+
+$("#button-results").click(function(){
+$("#results").toggleClass("special");
+});
